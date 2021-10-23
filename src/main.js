723402c3510183f004f15c200a98a3da4331d442
quockhangtran2001 <90443739+quockhangtran2001@users.noreply.github.com>
2021-10-23 22:24:08 +0700

    Lab 3 & 4
    Final

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-createApp(App).mount('#app')
+import { handle } from "./common/promise";
+import router from "./router";
+
+createApp(App)
+  .mixin({
+    methods: {
+      handle,
+    }
+  })
+  .use(router)
+  .mount('#app');
+
